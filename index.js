42bccd10d1b9be261d70c01576ff24c3ccb8ad25
mohamedelgallad-510 <mohamed.elgallad9@gmail.com>
2025-12-21 18:40:43 +0300

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,16 @@ function renderEmotionsRadios(cats){
     let emotionsString = ""
     
     for (let emotion of emotions) {
-        emotionsString += `<p>${emotion}</p>`
+        emotionsString += `
+        <div class='radio'>
+            <label for='${emotion}'>${emotion}</label>
+            <input
+            type='radio'
+            id='${emotion}'
+            value='${emotion}'
+            name='emotions-radio'
+            >
+        </div>` 
     }
     
     emotionRadios.innerHTML = emotionsString
