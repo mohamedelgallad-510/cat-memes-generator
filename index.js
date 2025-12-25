7f89daa852c1e80cfc90635c44f230c7d7eec42a
mohamedelgallad-510 <mohamed.elgallad9@gmail.com>
2025-12-25 22:42:25 +0300

    Update index.js

--- index.js
+++ index.js
@@ -13,6 +13,26 @@ function getEmotionsArray(cats){
 }
 
 const emotionRadios = document.getElementById('emotion-radios')
+
+emotionRadios.addEventListener('change', highlightCheckedOption)
+
+function highlightCheckedOption(e){
+/*
+Challenge:
+1. Create an array of all items that have 
+   the "radio" class.
+2. Iterate over the array and remove the 
+   "highlight" class from each one.
+*/ 
+    const radios = document.getElementsByClassName('radio')
+    
+    for (let radio of radios){
+        radio.classList.remove('highlight')
+    }
+
+    document.getElementById(e.target.id).parentElement.classList.add('highlight')
+}
+
 function renderEmotionsRadios(cats){
     const emotions = getEmotionsArray(cats)
     let emotionsString = ""
