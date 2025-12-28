704e0b3f124bc0e5948b6793b9ed4666a3327168
mohamedelgallad-510 <mohamed.elgallad9@gmail.com>
2025-12-28 21:51:12 +0200

    Update cat filtering logic for GIF option
    Filter matching cats based on GIF option.

--- index.js
+++ index.js
@@ -65,9 +65,12 @@ function getMatchingCatsArray() {
 
     const isGif = gifsOnlyOption.checked
     const matchingCatsArray = catsData.filter(function(cat){
-            return cat.emotionTags.includes(selectedEmotion)
-    })
-        
+            if (isGif) {
+                return (cat.emotionTags.includes(selectedEmotion) && cat.isGif)
+            } else {
+                return cat.emotionTags.includes(selectedEmotion)
+               }
+            })
     console.log(matchingCatsArray)
     console.log(isGif)
 }
